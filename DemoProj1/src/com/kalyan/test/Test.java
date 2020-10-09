38064867124fc7a305ef29b83adfe341cae306ea
KALYAN <KALYAN@192.168.0.7>
2020-10-09 12:27:55 +0530

    KALYAN DEV1 modified TEst1.java

--- DemoProj1/src/com/kalyan/test/Test.java
+++ DemoProj1/src/com/kalyan/test/Test.java
@@ -1,9 +1,13 @@
 package com.kalyan.test;
 
+import com.kalyan.beans.utility;
+
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello:: kalyan");
+		utility u=new utility();
+		System.out.println(u.getSysDate());
 
 	}
 
